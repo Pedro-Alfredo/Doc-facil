0b52c1ec78551e2f9b52882ca910613defb82d75
Pedro Alfredo <cuinhanepedro250@gmail.com>
2026-04-22 18:45:01 +0200

    Fix import path for global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import "@/styles/globals.css";
-
+import "../styles/globals.css";
 export const metadata = {
   title: "DocFácil - Empregos & Concursos",
   description: "Portal de empregos e concursos em Moçambique e África Austral"
